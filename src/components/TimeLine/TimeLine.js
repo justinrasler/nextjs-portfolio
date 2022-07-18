bc871f1e6b04356fc43e5dd99bff146fbe1950be
justinrasler <85715506+justinrasler@users.noreply.github.com>
2022-07-18 10:05:53 -0400

    about me updateds

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        I am General Assembly Bootcamp graduate from Atlanta, Ga trying to break into the tech field. I have a passion for technology and learning. Looking for my next opportunity to grow.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
